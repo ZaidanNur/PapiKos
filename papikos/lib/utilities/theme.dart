c173a753921b01423a8ab7059f8419d4f325d8e5
ZaidanNur <zaixyz332@gmail.com>
2021-09-28 11:35:38 +0700

    add images & crete theme.dart

---
+++ papikos/lib/utilities/theme.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Color Pallete
+Color purpleMain = const Color(0xff5843BE);
+Color softOrange = const Color(0xffFF9376);
+Color textGray = const Color(0xff82868E);
+Color iconGray = const Color(0xff989BA1);
+Color bottomNavPurple = const Color(0xff7F74EB);
+Color bottomNavGray = const Color(0xffF6F7F8);
+Color black = const Color(0xff000000);
+
+// Text Style
+TextStyle heading1 = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  fontSize: 24,
+  color: black,
+);
+TextStyle subHeading1 = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 16,
+  color: textGray,
+);
+TextStyle cardLevel1 = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  fontSize: 18,
+  color: black,
+);
+TextStyle cardLevel2 = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 16,
+  color: textGray,
+);
+TextStyle cardLevel3 = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 14,
+  color: textGray,
+);
